a34191cb9df4ff008bbe5173625467945c0bc5b9
iqrarafiq12 <iqrak0128@gmail.com>
2023-12-23 10:55:35 -1200

    My Story Section Has been completed ✅

--- app/(main)/_components/About.tsx
+++ app/(main)/_components/About.tsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React from "react";
 
-function About() {
+const About = () => {
   return (
-    <div className='flex item-center justify-center flex-col py-20'>
-    <div className='text-center'>
-      <h1 className='text-2xl md:text-4xl mb-1 md:mb-3 text-cyan-800 dark:text-cyan-200 '>Hi, This Is Iqra</h1>
-      <p className='text-md md:text-xl mb-3 text-cyan-600 dark:text-cyan-200'>Clean code always looks like it was written by someone who cares.</p>
-      <a href='./WorksItem' className="text-gray-700 bg-gradient-to-r from-cyan-400 to-lime-400 hover:bg-gradient-to-l hover:from-cyan-300 hover:to-lime-300 focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">See Work</a>
-    </div>
+    <section
+      id="about"
+      className="flex flex-col items-center justify-center h-full relative overflow-hidden pt-[35px] mt-[-80px]"
+      style={{ transform: "scale(0.9" }}
+    >
+      <div className="flex flex-col  justify-around flex-wrap items-center   max-w-[900px]">
+        <h1 className="text-zinc-700 cursor-pointer hover:-translate-y-1 transition hover:scale-110 ease-in-out hover:text-transparent bg-clip-text hover:bg-gradient-to-r from-purple-500 to-orange-400 z-[1] tracking-tighter text-7xl font-semibold dark:text-white max-w-[600px] w-auto h-auto pt-28">
+          ABOUT ME
+        </h1>
+        <p className="tracking-[0.5em] pb-6 font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500  text-1xl">
+          EXPLORE NOW
+        </p>
+        <p className="text-zinc-600 text-center dark:text-zinc-200 ">
+          Hailing from Karachi, Pakistan, I am a dedicated software developer
+          with a foundation rooted in web development, which I began honing
+          under the guidance of the esteemed 10 Pearls company. My commitment to
+          professional growth led me to attain certification in Web and App
+          Hybrid Development in 2023 from UIT university. Since then, I have
+          fervently pursued my career as a programmer, embracing every
+          opportunity to further my expertise.
+          <br />
+          <br />
+          Venturing into the realm of social media, I have established myself as
+          a content creator, actively growing my channel and crafting content to
+          benefit aspiring developers. My primary objectives encompass both
+          personal and collective growth: I am determined to amplify my skill
+          set, collaborate with leading companies to gain invaluable experience,
+          and most importantly, extend my support to individuals navigating
+          challenges in their careers.
+          <br />
+          <br />
+          In terms of technical proficiency, I am well-versed in a myriad of
+          languages and tools. Notably, I have cultivated expertise in Next.js,
+          Tailwind CSS, and TypeScript, which form the cornerstone of my
+          development project&#39;s.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-  </div>
-  )
-}
-
-export default About
+export default About;
--- app/(main)/_components/hero/Hero.tsx
+++ app/(main)/_components/hero/Hero.tsx
@@ -1,20 +1,19 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
-const Hero = () => {
+function Hero() {
   return (
-    <div className="flex flex-col items-center justify-center px-20 z-[20] ">
-      <div className="flex flex-col  justify-center  text-center">
-        <div className="flex text-zinc-700 flex-col gap-6 mt-[50px] cursor-pointer hover:-translate-y-1  transition  hover:scale-110  ease-in-out hover:text-transparent bg-clip-text  hover:bg-gradient-to-r from-purple-500 to-orange-400 z-[1] tracking-tighter text-7xl font-semibold dark:text-white max-w-[600px] w-auto h-auto">
-         IQRA RAFIQ
+    <div className="flex flex-col md:flex-row lg:flex-col xl:flex-row justify-center items-center min-h-screen gap-5 max-container mx-auto p-10 z-30">
+      
+      <div className="relative w-full md:w-auto lg:w-full xl:w-auto mb-10 md:mb-0 lg:mb-10 xl:mb-0">
+        <div className="text-zinc-700 cursor-pointer hover:-translate-y-1 transition hover:scale-110 ease-in-out hover:text-transparent bg-clip-text hover:bg-gradient-to-r from-purple-500 to-orange-400 z-[1] tracking-tighter text-7xl font-semibold dark:text-white max-w-[600px] w-auto h-auto pt-28">
+          IQRA RAFIQ
         </div>
         <p className="text-2xl font-medium tracking-tighter text-zinc-500 dark:text-zinc-300 max-w-[600px]">
           I do Code &{" "}
-          <span className="text-transparent font-semibold  bg-clip-text bg-gradient-to-r from-purple-500 to-orange-400">
-            Chill
+          <span className="text-transparent font-semibold bg-clip-text bg-gradient-to-r from-purple-500 to-orange-400">
+            Create Content
           </span>{" "}
           🍿
         </p>
@@ -22,21 +21,22 @@ const Hero = () => {
           Passionate Software Developer with a focus on Full Stack development,
           dedicated to crafting elegant and user-friendly Web & App applications.
         </p>
-        <div className="text-md flex justify-center">
+        <div className="text-md justify-center">
           <Button
-            onClick={() => window.open("mailto:iqrak0128@gmail.com")}
             variant="outline"
-            className="z-[1] padding-20 text-black dark:text-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black font-semibold hover:border-transparent"
+            className="z-[1] p-4 text-black dark:text-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black font-semibold hover:border-transparent"
           >
             Contact Me
           </Button>
         </div>
-        <div className="justify-center flex ">
-          <Image src="/Assets/Hero-Img.svg" height={500} width={600} alt="Iqra Rafiq" />
-        </div>
       </div>
+      
+      <div className="justify-center items-center w-full max-w-[500px]">
+        <Image src="/Assets/Hero-Img.svg" height={700} width={700} alt="Iqra Rafiq"/>
+      </div>
+      
     </div>
-  );
-};
+  )
+}
 
 export default Hero;
